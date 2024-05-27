2024cb0d309c8a482902c01b30c82692712d649b
remitoudic <remitoudic@users.noreply.github.com>
2024-05-27 08:33:54 +0000

    update Q5

--- week2/homework/hpo.py
+++ week2/homework/hpo.py
@@ -41,8 +41,10 @@ def run_optimization(data_path: str, num_trials: int):
             rf.fit(X_train, y_train)
             y_pred = rf.predict(X_val)
             rmse = mean_squared_error(y_val, y_pred, squared=False)
-            print(rmse)
+        
             mlflow.log_metric("rmse",rmse)
+            # best rmse 5.35
+            
 
         return {'loss': rmse, 'status': STATUS_OK}
 
